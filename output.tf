56e7fa1020facb00f01291f3e23759f67c869a01
sumitthakur2203 <128373003+sumitthakur2203@users.noreply.github.com>
2023-08-12 20:01:17 +0530

    output.tf has been deleted

--- output.tf
+++
@@ -1,9 +0,0 @@
-output "VM_name" {
-    value = google_compute_instance.vm.name
-}
-output "VM_machine_type" {
-    value = google_compute_instance.vm.machine_type
-}
-output "VM_zone" {
-    value = google_compute_instance.vm.zone
-}
